0f43cfae30a1c290d2cf41e7f0eb9c6bae6dc6a6
Rishabh Prakash <rishuprakash2002@gmail.com>
2023-01-27 23:54:30 +0530

    React Query

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,31 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+  const result = useQuery(["details", id], fetchPet);
+
+  if (result.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🐬</h2>
+      </div>
+    );
+  }
+
+  const pet = result.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
