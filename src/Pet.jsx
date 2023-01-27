ffc9d269b9e9d5f096ee2e8eef326cd6e2328e30
Rishabh Prakash <rishuprakash2002@gmail.com>
2023-01-27 23:37:48 +0530

    React Router Dom

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Pet = (props) => {
   const { name, animal, breed, images, location, id } = props;
 
@@ -6,7 +8,7 @@ const Pet = (props) => {
     hero = images[0];
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -14,7 +16,7 @@ const Pet = (props) => {
         <h1>{name}</h1>
         <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
